c3955184c2eeef424aa3779b39f5aa5dbfc27f37
BenjWills <bunjw.bw@gmail.com>
2024-03-02 11:32:31 +0000

    new input system broke

--- Assets/Scripts/InputManagerScript.cs
+++ Assets/Scripts/InputManagerScript.cs
@@ -10,6 +10,9 @@ public class InputManagerScript : MonoBehaviour
     public GameObject pauseMenu;
     public GameObject settingsMenu;
     public InGameMenuSettings iGMenuSettings;
+    private Vector2 moveInput;
+    public Rigidbody rb;
+    [SerializeField] float movementSpeed = 10f;
 
     // Start is called before the first frame update
     void Start()
@@ -20,7 +23,7 @@ public class InputManagerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        Movement();
     }
 
     public void PauseUnpause(InputAction.CallbackContext context)
@@ -34,5 +37,16 @@ public class InputManagerScript : MonoBehaviour
         {
             iGMenuSettings.ResumeGame();
         }
+    }
+
+    public void Movement()
+    {
+        Vector3 playerVelocity = new Vector3(moveInput.x * movementSpeed, rb.velocity.y, moveInput.y * movementSpeed);
+        rb.velocity = transform.TransformDirection(playerVelocity);
+    }
+
+    void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
     }
 }
